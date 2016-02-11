20a8e4b023981d345cbc0b18b56468a265b676e3
PAUL <paul-young@live.co.uk>
2016-02-11 01:14:04 +0000

    PostgreSQL change
    The database has been changed to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160210160135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exchange_rates", force: :cascade do |t|
     t.string   "date"
     t.decimal  "rate"
